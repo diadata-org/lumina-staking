840320881bd4bb01515327625ad2fac8d32cb7e1
nnn-gif <nicky.gurbani@gmail.com>
2025-06-02 08:00:04 +0530

    fix: remove duplicate import

--- contracts/DIAExternalStaking.sol
+++ contracts/DIAExternalStaking.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.29;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./StakingErrorsAndEvents.sol";
